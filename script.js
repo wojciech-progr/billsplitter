a74c0de95bedb0553b31bc45a5691eeb70cd3042
Wojtek <wojciech.progr@gmail.com>
2023-10-22 00:58:17 +0200

    Final bill counting

--- script.js
+++ script.js
@@ -7,11 +7,25 @@ const costInfo = document.querySelector('.cost-info');
 const cost = document.querySelector('.cost');
 
 const showBill = () => {
-
     if (price.value == '' || people.value == '' || tip.value == 0) {
         error.textContent = 'Uzupełnij wszystkie pola';
+        costInfo.style.display = 'none';
+    } else {
+        /* clear error */
+        error.textContent = '';
+        countBill();
     }
+}
 
+const countBill = () => {
+    const newPrice = parseFloat(price.value);
+    const newPeople = parseInt(people.value);
+    const newTip = parseFloat(tip.value);
+
+    const sum = (newPrice + newPrice * newTip) / newPeople;
+    costInfo.style.display = 'block';
+
+    cost.textContent = sum.toFixed(2);
 }
 
 countBtn.addEventListener('click', showBill);
